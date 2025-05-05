4224b748ebfd7c7e949f55c4c0b0353d38a8114f
mjsb05 <maurijsb25@gmail.com>
2025-05-04 18:25:38 -0600

    Stream Response

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -1,14 +1,17 @@
-import { GoogleGenAI } from "@google/genai";
 import { Injectable } from '@nestjs/common';
 import { BasicPromptDto } from './dtos/basic-prompt.dto';
-import { basicPromptUseCase } from "./use-cases/basic-prompt.use-case";
-
+import { GoogleGenAI } from '@google/genai';
+import { basicPromptUseCase } from './use-cases/basic-prompt.use-case';
+import { basicPromptStreamUseCase } from './use-cases/basic-prompt-Stream.use-case';
 @Injectable()
 export class GeminiService {
-
   private ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
- async basicPrompt(basicPromptDto: BasicPromptDto) {
-  return basicPromptUseCase(this.ai, basicPromptDto);
- }
+  async basicPrompt(basicPromptDto: BasicPromptDto) {
+    return basicPromptUseCase(this.ai, basicPromptDto);
+  }
+
+  async basicPromptStream(basicPromptDto: BasicPromptDto) {
+    return basicPromptStreamUseCase(this.ai, basicPromptDto);
+  }
 }
