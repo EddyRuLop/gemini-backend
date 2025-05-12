c3825319447a54b77023407efda1f055c0e2559a
Eduardo Ruiz <eduardoyahel@gmail.com>
2025-05-11 23:58:13 -0600

    Conversaciones en varios turnos

--- src/gemini/gemini.controller.ts
+++ src/gemini/gemini.controller.ts
@@ -13,10 +13,30 @@ import { Response } from 'express';
 
 import { GeminiService } from './gemini.service';
 import { BasicPromptDto } from './dtos/basic-prompt.dto';
+import { GenerateContentResponse } from '@google/genai';
+import { ChatPromptDto } from './dtos/chat-prompt.dto';
 
 @Controller('gemini')
 export class GeminiController {
   constructor(private readonly geminiService: GeminiService) {}
+
+
+  async outputStreamResponse(res: Response, stream: AsyncGenerator<GenerateContentResponse, any, any> ){
+    // res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(HttpStatus.OK);
+
+
+let resultText = '';
+    for await (const chunk of stream) {
+      const piece = chunk.text;
+      resultText += piece;
+      res.write(piece);
+    }
+
+    res.end();
+    return resultText;
+  }
 
   @Post('basic-prompt')
   basicPrompt(@Body() basicPromptDto: BasicPromptDto) {
@@ -35,17 +55,25 @@ export class GeminiController {
     console.log(files);
 
     const stream = await this.geminiService.basicPromptStream(basicPromptDto);
+    void this.outputStreamResponse(res, stream);
+   
+  }
 
-    // res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(HttpStatus.OK);
+  @Post('chat-stream')
+  @UseInterceptors(FilesInterceptor('files'))
+  async chatStream(
+    @Body() chatPromptDto: ChatPromptDto,
+    @Res() res: Response,
+    @UploadedFiles() files: Array<Express.Multer.File>,
+  ) {
+    chatPromptDto.files = files;
 
-    for await (const chunk of stream) {
-      const piece = chunk.text;
-      console.log(piece);
-      res.write(piece);
-    }
+    console.log(files);
 
-    res.end();
+    const stream = await this.geminiService.basicPromptStream(chatPromptDto);
+
+    const data = await this.outputStreamResponse(res, stream);
+
+    console.log(data);
   }
 }
